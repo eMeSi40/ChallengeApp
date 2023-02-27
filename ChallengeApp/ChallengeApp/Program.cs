3b565b79999c472f891ab1e4a7f033003300d673
Lech Kuciński <kucinski31@gmail.com>
2023-02-27 22:20:06 +0100

    Zadanie domowe - dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// Zadanie domowe - dzień 4
+
+string name = "Ewa";
+var age = 17;
+bool women = true;
+
+if (women == true && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+
+else if (name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+
+else if (women == false && age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+
+else
+{
+    Console.WriteLine("Nie spełnia żadnych kryteriów");
+}
+
+
+
+
